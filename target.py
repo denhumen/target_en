3167cacc48711808efab3793c0639f171f226bd6
denhumen <humeniuk.pn@ucu.edu.ua>
2022-11-03 01:52:30 +0200

    fourth

--- target.py
+++ target.py
@@ -31,22 +31,19 @@ def get_words(file: str, letters: List[str]) -> List[str]:
     """
     Reads the file file. Checks the words with rules and returns a list of words.
     """
+    words = []
     with open(file, 'r', encoding='utf-8') as input_file:
         words = input_file.readlines()
     words = [word.strip().lower() for word in words]
     answer_list = []
-    for i in words:
-        if i.count(letters[4]) == 0:
-            continue
-        counter = 0
-        for j in i:
-            if letters.count(j) == 0:
-                counter += 1
-            elif i.count(j) > letters.count(j):
-                counter += 1
-        if counter > 0:
-            continue
-        answer_list.append(i)
+    for word in words:
+        if letters[4] in word and len(word) > 3:
+            bad_counter = 0
+            for letter in word:
+                if word.count(letter) > letters.count(letter) or not letter in letters:
+                    bad_counter += 1
+            if bad_counter == 0 and word not in answer_list:
+                answer_list.append(word)
     return answer_list
 
 def get_user_words() -> List[str]:
@@ -76,35 +73,9 @@ def get_pure_user_words(user_words: List[str],
     that are not in dictionary.
     """
     answer_list = []
-    letters_tuple = set()
-    for i in letters:
-        letters_tuple.add((i, letters.count(i)))
-    letters_tuple = sorted(list(letters_tuple))
-    string_letters = ''
-    for tuple_ in letters_tuple:
-        string_letters += tuple_[0]
-    for i in user_words:
-        if i.count(letters[4]) == 0:
-            continue
-        word_tuple = set()
-        for j in i:
-            word_tuple.add((j, i.count(j)))
-        word_tuple = sorted(list(word_tuple))
-        bad_counter = 0
-        for j in word_tuple:
-            if string_letters.count(j[0]) == 0:
-                bad_counter += 1
-            for k in letters_tuple:
-                if j[0] == k[0] and j[1] > k[1]:
-                    bad_counter += 1
-        if bad_counter > 0:
-            continue
-        if i not in words_from_dict:
-            answer_list.append(i)
     return answer_list
 
 def results():
     """
     Function
     """
-    pass
